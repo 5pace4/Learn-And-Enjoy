d0a0546576fa11bc169c41b4c90a2a91c2e9bbec
TofayelAhmmedBabu <81514847+TofayelAhmmedBabu@users.noreply.github.com>
2021-09-22 17:50:22 +0600

    Add files via upload
    //addition of big number//

---
+++ aad.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#include<cstring>
+using namespace std;
+using ll=long long;
+string bigSum(string s1, string s2){
+    string sum="";
+    if(s1.size()>s2.size()){
+        swap(s1, s2);
+    }
+    int ln1=s1.size(), ln2= s2.size(), intsum=0, carry=0;
+     int digitdiffer=ln2-ln1;
+    for(int i=ln1-1; i>=0; i--){
+        intsum=(s1[i]-'0')+(s2[i+digitdiffer]-'0')+carry;
+        sum.push_back(intsum%10+'0');
+        carry=intsum/10;       
+    }
+    if(carry)
+      sum.push_back(carry+'0');
+
+    reverse(sum.begin(), sum.end());
+    return sum;
+}
+int main(){
+      string s1, s2;
+      cin>>s1>>s2;
+      cout<<bigSum(s1, s2)<<endl;
+
+}
