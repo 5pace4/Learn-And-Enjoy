a9b9b6af5b3bddbffce482939dbbd25556d754df
Tofayel Ahmmed Babu <81514847+TofayelAhmmedBabu@users.noreply.github.com>
2022-07-15 09:37:15 +0600

    Create blind_eyes.cpp

---
+++ CP Template/blind_eyes.cpp
@@ -0,0 +1,23 @@
+/*                  
+       |      |            |
+       |____  | 0 ___  ____|        ____ _    _ ____  ___
+       |    | | ||   ||    |       |____| \  / |____||___
+       |____| | ||   ||____|_______|____   \/  |____  ___|
+                                           /   
+                                          /
+*/                                                     
+
+#include<bits/stdc++.h>
+
+using namespace std;
+#define int long long
+void f() {
+   
+}
+int32_t main() {
+     ios_base::sync_with_stdio(0);cin.tie(0);
+     int t; cin >> t;
+     while(t-->0)
+          f();
+     return 0;
+}
